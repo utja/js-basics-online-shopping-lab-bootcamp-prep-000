06de6b3be2bfa766484eb9dcd79609d90ac70f42
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-14 02:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber){
-    console.log(`Your total cost is $${total()}, which will be charged to card ${cardNumber}.`)
+    console.log(`Your total cost is $${total()}, which will be charged to card ${cardNumber}.`);
+    return(`Your total cost is $${total()}, which will be charged to card ${cardNumber}.`);
     for (var i = cart.length; i >= 0; i--) {
       cart.pop();
     }
