a406707442731c73341c993ba433e5f6c9eb25cf
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-14 00:22:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function viewCart() {
   var inCart = 'In your cart, you have '
   if (cart.length === 0 ) {
     console.log('Your shopping cart is empty.')
+    return('Your shopping cart is empty.')
   } else if (cart.length === 1){
     console.log(inCart + `${Object.values(cart[0])} at ${Object.values(cart[0])}.`);
   }
