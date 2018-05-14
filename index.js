8ad2f511aa31eec5f977f5d579c72d87baa95914
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-14 00:01:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var cart = [];
 
+function getRandomInt(min,max) {
+  return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
+function generateCartItem(itemName) {
+  // generates the two key value pairs with itemname and itemprice in front
+  return {
+    itemName:itemName,
+    itemPrice:getRandomInt(1, 100)
+  }
+}
+
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -11,6 +23,17 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var item1 = generateCartItem(item);
+/* var price = Math.floor(Math.random()*100);
+ var itemObject = {};
+ var name = `itemName: ${item}`;
+ function listPrice(price) {
+   return `itemPrice: ${price}`
+ }
+ itemObject[name] = listPrice(price);
+ */
+ cart.push(item1);
+ return console.log(`${item} has been added to your cart.`);
 }
 
 function viewCart() {
